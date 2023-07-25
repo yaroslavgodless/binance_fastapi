810e7d81221bf0d07e59afcdc16418696faf7de3
Yaroslav Khoda <yaroslavkhoda@MacBook-Air-Yaroslav.local>
2023-07-25 03:00:57 +0300

    simple FastAPI app with Redis

---
+++ app/internal/routes/currency.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+from app.pkg.redis_tools.tools import RedisTools
+
+router = APIRouter(
+    prefix='/api/v1/currency'
+)
+
+@router.get('/{pair}')
+def get_currency_pair(pair: str):
+    
+    if pair not in [s.decode('utf-8') for s in RedisTools.get_keys()]:
+        return {
+            'error': 'This pair does not exists'
+        }
+    
+    return {
+        'pair': pair,
+        'price': RedisTools.get_pair(pair),
+    }
---
+++ app/internal/schemas/currency.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+from typing import List
+
+class Symbol(BaseModel):
+    symbol: str
+
+class Symbols(BaseModel):
+    symbols: List[Symbol]
